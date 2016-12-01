57bd9ec5553d6a060dcc45f7c3e7a0d65563e823
Jason Post <jason.w.post@gmail.com>
2016-12-01 15:30:09 +1300

    make angular models

--- routes/app.js
+++ routes/app.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-      if (err){
-        return res.send('Error!');
-      }
-      res.render('node', {email: doc.email});
-    });
+      res.render('index');
 });
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-      firstName:'Jason',
-      lastName:'Post',
-      password:'super-secret',
-      email: email
-    });
-    user.save(function(err, result){
-      if (err){
-        console.log(err);
-    }
-    });
-    res.redirect('/');
-});
-
 
 module.exports = router;
