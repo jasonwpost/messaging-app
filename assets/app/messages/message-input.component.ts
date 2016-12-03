29e8c648c56f2928a982c024b1a289ea194cea75
Jason Post <jason.w.post@gmail.com>
2016-12-03 17:50:29 +1300

    connect message front-end to back-end

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
@@ -7,13 +7,44 @@ import { Message } from "./message.model";
   selector: 'app-message-input',
   templateUrl: './message-input.component.html',
 })
-export class MessageInputComponent {
+export class MessageInputComponent implements OnInit {
+  message: Message;
 
   constructor(private messageService: MessageService){}
 
   onSubmit(form: NgForm){
-    const message = new Message(form.value.content, 'Max');
-    this.messageService.addMessage(message);
+    if (this.message){
+      //edit
+      this.message.content = form.value.content;
+      this.messageService.updateMessage(this.message)
+        .subscribe(
+          result => console.log(result)
+        );
+      this.message = null;
+    } else {
+      //create
+      const message = new Message(form.value.content, 'Max');
+      this.messageService.addMessage(message)
+      .subscribe(
+        // three arguments
+        //success case
+        data => console.log(data),
+        // error case
+        error => console.error(error)
+        // complete function - not shown here
+      );
+    }
     form.resetForm();
+  }
+
+  onClear(form: NgForm){
+    this.message = null;
+    form.resetForm();
+  }
+
+  ngOnInit(){
+    this.messageService.messageIsEdit.subscribe(
+      (message: Message) => this.message = message
+    );
   }
 }
