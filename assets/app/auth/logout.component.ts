07953c3dcfe72c23785118365e981c77dd20a31e
Jason Post <jason.w.post@gmail.com>
2016-12-04 16:15:16 +1300

    add logout functionality

--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -9,5 +11,10 @@ import { Component } from '@angular/core';
   `
 })
 export class LogoutComponent {
-  onLogout(){}
+
+  constructor(private authService: AuthService, private router: Router) {}
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth', 'signin']);
+  }
 }
