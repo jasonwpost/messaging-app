98c140ad9b6e39f91c850d45421c326b0855452a
Jason Post <jason.w.post@gmail.com>
2016-12-05 17:21:48 +1300

    set up backend authorization

--- models/message.js
+++ models/message.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var User = require('./user');
+
 var schema = new Schema({
   content: {type: String, required: true},
   user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+//post: happens after some action
+// here: remove
+schema.post('remove', function(message){
+  User.findById(message.user, function(){
+    user.messages.pull(message);
+  });
 });
 
 module.exports = mongoose.model('message', schema);
